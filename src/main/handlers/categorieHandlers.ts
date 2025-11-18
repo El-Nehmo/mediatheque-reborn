3b79f2e3bf95f3f26dfe75c9e572333680a451d5
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-11-18 13:53:55 +0100

    feat: add categorieHandlers for IPC communication

---
+++ src/main/handlers/categorieHandlers.ts
@@ -0,0 +1,28 @@
+// Handlers IPC pour les catégories
+import { ipcMain } from 'electron';
+import { CategorieService } from '../services/categorieService';
+
+const categorieService = new CategorieService();
+
+export function registerCategorieHandlers() {
+    
+  ipcMain.handle('categorie:getAll', async () => {
+    return await categorieService.getAllCategories();
+  });
+
+  ipcMain.handle('categorie:getById', async (event, id: number) => {
+    return await categorieService.getCategorieById(id);
+  });
+
+  ipcMain.handle('categorie:create', async (event, data) => {
+    return await categorieService.createCategorie(data);
+  });
+
+  ipcMain.handle('categorie:update', async (event, id: number, data) => {
+    return await categorieService.updateCategorie(id, data);
+  });
+
+  ipcMain.handle('categorie:delete', async (event, id: number) => {
+    return await categorieService.deleteCategorie(id);
+  });
+}
