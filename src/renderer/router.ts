8cda9cf4f89cc7370f7c0f84f74f0c440af99b23
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-11-19 23:22:55 +0100

    feat: add /categories route to Vue router

--- src/renderer/router.ts
+++ src/renderer/router.ts
@@ -3,11 +3,13 @@ import { createRouter, createMemoryHistory } from "vue-router";
 import Home from "./pages/Home.vue";
 import Albums from "./pages/Albums.vue";
 import AlbumDetail from "./pages/AlbumDetail.vue";
+import Categories from "./pages/Categories.vue";
 
 const routes = [
     { path: '/', component: Home },
     { path: '/albums', component: Albums },
-    { path: '/albums/:id_album', component: AlbumDetail }
+    { path: '/albums/:id_album', component: AlbumDetail },
+    { path: '/categories', component: Categories }
 ]
 
 export const router = createRouter({routes, history: createMemoryHistory()});
