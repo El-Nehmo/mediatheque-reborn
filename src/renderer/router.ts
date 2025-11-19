53eec84e23ca4bdcf6ba2793bea4b17987b74fea
Néhémie Mubenga Maweja <132246535+El-Nehmo@users.noreply.github.com>
2025-11-19 23:03:57 +0100

    feat: add dynamic album detail route to Vue router

--- src/renderer/router.ts
+++ src/renderer/router.ts
@@ -1,9 +1,13 @@
 import { createRouter, createMemoryHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
+import Albums from "./pages/Albums.vue";
+import AlbumDetail from "./pages/AlbumDetail.vue";
 
 const routes = [
-    { path: '/', component: Home}
+    { path: '/', component: Home },
+    { path: '/albums', component: Albums },
+    { path: '/albums/:id_album', component: AlbumDetail }
 ]
 
 export const router = createRouter({routes, history: createMemoryHistory()});
